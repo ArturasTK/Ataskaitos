54a07a7598ee2d31fbbf2af7ff6f9bc0f77425a3
Artūras Tkačiovas <arturas.tkaciovas@gmail.com>
2022-07-24 23:03:15 +0300

    sukurtas produkto modelis

--- ataskaitos/models.py
+++ ataskaitos/models.py
@@ -1,3 +1,28 @@
+from audioop import ratecv
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    product_name = models.CharField('Produktas', max_length=50)
+    price = models.DecimalField('Įkainis', max_digits=10, decimal_places=2) 
+
+    def __str__(self):
+       return str(self.item) + ": $" + str(self.price)
+
+    
+
+# class Report(models.Model):
+#     date_field =
+#     dtk_nr =
+#     product_name = 
+#     remakes =
+#     number_of_plates =
+#     number_of_new_plates = 
+#     price = 
+#     total = 
+#     reasons =
+#     notes = 
+
+
+
+
+
