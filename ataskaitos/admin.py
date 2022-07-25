a9573ff38d3d29f9b3a7ced54328c64982954eec
Artūras Tkačiovas <arturas.tkaciovas@gmail.com>
2022-07-25 18:47:25 +0300

    Sukurti modeliai perdaymai priežastys ataskaita

--- ataskaitos/admin.py
+++ ataskaitos/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Report, Remake, Reason
 
 admin.site.register(Product)
+
+admin.site.register(Remake)
+
+admin.site.register(Reason)
+
+admin.site.register(Report)
--- ataskaitos/models.py
+++ ataskaitos/models.py
@@ -1,27 +1,42 @@
 from audioop import ratecv
 from django.db import models
+from datetime import date
 
 class Product(models.Model):
     product_name = models.CharField('Produktas', max_length=50)
     price = models.DecimalField('Įkainis', max_digits=10, decimal_places=2) 
 
     def __str__(self):
-       return str(self.item) + ": $" + str(self.price)
+       return str(self.product_name) + ": €" + str(self.price)
 
-    
 
-# class Report(models.Model):
-#     date_field =
-#     dtk_nr =
-#     product_name = 
-#     remakes =
-#     number_of_plates =
-#     number_of_new_plates = 
-#     price = 
-#     total = 
-#     reasons =
-#     notes = 
+class Remake(models.Model):
+    remakes = models.CharField('Perdarymai', max_length=50)
 
+    def __str__(self):
+        return str(self.remakes)
+
+
+class Reason(models.Model):
+    reasons = models.CharField('Priežastys', max_length=50)
+
+    def __str__(self):
+        return str(self.reasons)
+
+
+class Report(models.Model):
+    date_field = models.DateField('Data', default=date.today)
+    dtk_nr = models.CharField('DTK Nr.', max_length=10)
+    product_name = models.ForeignKey(Product, help_text='Pasirinkite produktą', on_delete=models.SET_NULL, null=True)
+    remakes = models.ForeignKey(Remake, help_text='Pasirinkite perdarimo priežastis', on_delete=models.SET_NULL, null=True)
+    # number_of_plates =
+    # number_of_new_plates = 
+    # price = ?
+    # total = 
+    reasons = models.ForeignKey(Reason, help_text='Pasirinkite korekcijos tipą', on_delete=models.SET_NULL, null=True)
+    notes = models.TextField('Pastabos', max_length=1000, help_text='Papildomi dabai')
+    def __str__(self):
+        return f'{self.date_field} {self.dtk_nr}'
 
 
 
