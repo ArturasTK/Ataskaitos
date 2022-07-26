fba37a804e1232f9d3b6bca5015b81232192a611
Artūras Tkačiovas <arturas.tkaciovas@gmail.com>
2022-07-26 18:16:28 +0300

    pabaigtas modelis Report

--- ataskaitos/models.py
+++ ataskaitos/models.py
@@ -2,6 +2,7 @@ from audioop import ratecv
 from django.db import models
 from datetime import date
 
+
 class Product(models.Model):
     product_name = models.CharField('Produktas', max_length=50)
     price = models.DecimalField('Įkainis', max_digits=10, decimal_places=2) 
@@ -29,10 +30,8 @@ class Report(models.Model):
     dtk_nr = models.CharField('DTK Nr.', max_length=10)
     product_name = models.ForeignKey(Product, help_text='Pasirinkite produktą', on_delete=models.SET_NULL, null=True)
     remakes = models.ForeignKey(Remake, help_text='Pasirinkite perdarimo priežastis', on_delete=models.SET_NULL, null=True)
-    # number_of_plates =
-    # number_of_new_plates = 
-    # price = ?
-    # total = 
+    number_of_plates = models.IntegerField('Klišių sk.')
+    number_of_new_plates = models.IntegerField('Keičiamų klišių sk.') 
     reasons = models.ForeignKey(Reason, help_text='Pasirinkite korekcijos tipą', on_delete=models.SET_NULL, null=True)
     notes = models.TextField('Pastabos', max_length=1000, help_text='Papildomi dabai')
     def __str__(self):
