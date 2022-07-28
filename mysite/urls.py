2d121da757789bec854af939899b666a81919bdd
Artūras Tkačiovas <arturas.tkaciovas@gmail.com>
2022-07-28 14:35:04 +0300

    pataisiau startini psl.

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('ataskaitos/', include('ataskaitos.urls')),
     path('admin/', admin.site.urls),
+    path('', RedirectView.as_view(url='ataskaitos/', permanent=True)),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
