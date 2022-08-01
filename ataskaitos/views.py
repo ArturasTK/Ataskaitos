5a9caf1076c9a540698c10da1fe7aeb110534d78
Artūras Tkačiovas <arturas.tkaciovas@gmail.com>
2022-08-01 22:18:27 +0300

    pridėtas sesijų saugojimas

--- ataskaitos/views.py
+++ ataskaitos/views.py
@@ -8,8 +8,11 @@ from django.core.paginator import Paginator
 def index(request):
 
     num_reports = Report.objects.all().count()
+    num_visits = request.session.get('num_visits', 1)
+    request.session['num_visits'] = num_visits + 1
     context = {
         'num_reports': num_reports,
+        'num_visits': num_visits,
     }
 
     return render(request, 'index.html', context=context)
