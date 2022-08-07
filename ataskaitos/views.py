bed589635ac60416a95a75db3040cb216ced241d
Artūras Tkačiovas <arturas.tkaciovas@gmail.com>
2022-08-07 18:29:40 +0300

    pataisyta, kad suvedus duomenis forma išsivalytu

--- ataskaitos/views.py
+++ ataskaitos/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 # from matplotlib.style import context
 from .models import Product, Report, Remake, Reason
 # from django.views import generic
@@ -20,14 +21,25 @@ def index(request):
     return render(request, 'index.html', context=context)
 
 def report(request):
+    # submitted = False
+    if request.method == "POST":
+        form = ReportForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # return render(request, 'report.html', submitted=True)
 
-    form = ReportForm    
+            return HttpResponseRedirect('/ataskaitos/report/')
+    else:
+        form = ReportForm() 
+    #     if 'submitted' in request.GET:
+    #         submitted = True   
     paginator = Paginator(Report.objects.filter(user=request.user).order_by('-date_field'), 10)
     page_number = request.GET.get('page')
     paged_reports = paginator.get_page(page_number)
     context = {
         'report': paged_reports,
-        'form': form, 
+        'form': form,
+        # 'submitted': submitted, 
     }
     
     return render(request, 'report.html', context=context)
