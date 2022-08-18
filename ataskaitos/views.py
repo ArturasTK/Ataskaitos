9ff95f691c294da4c8de77fa43452ac0b05459fd
Artūras Tkačiovas <arturas.tkaciovas@gmail.com>
2022-08-18 19:06:22 +0300

    kankinu vartotojo rezultato skaičivimą

--- ataskaitos/views.py
+++ ataskaitos/views.py
@@ -4,6 +4,7 @@ from .models import Product, Report, Remake, Reason
 from django.core.paginator import Paginator
 from .forms import ReportForm
 from datetime import datetime
+from django.db.models import Sum
 
 # def count(request):
 #     month_value = Report.
@@ -11,8 +12,10 @@ from datetime import datetime
 def base(request):
 
     num_reports_of_day = Report.objects.filter(date_field__contains=datetime.today().date()).count()
+    num_reports_of_month = Report.objects.filter(user=request.user).filter(date_field__contains=datetime.now().month).aggregate(Sum('number_of_new_plates'))
     context = {
         'num_reports_of_day': num_reports_of_day,
+        'num_reports_of_month':num_reports_of_month,
     }
 
     return render(request, 'base.html', context=context)
@@ -21,12 +24,14 @@ def index(request):
 
     num_reports = Report.objects.all().count()
     num_reports_of_day = Report.objects.filter(date_field__contains=datetime.today().date()).count()
+    num_reports_of_month = Report.objects.filter(user=request.user).filter(date_field__contains=datetime.now().month).aggregate(Sum('number_of_new_plates'))
     num_visits = request.session.get('num_visits', 1)
     request.session['num_visits'] = num_visits + 1
     context = {
         'num_reports': num_reports,
         'num_visits': num_visits, 
-        'num_reports_of_day': num_reports_of_day,       
+        'num_reports_of_day': num_reports_of_day,
+        'num_reports_of_month':num_reports_of_month,               
     }
 
     return render(request, 'index.html', context=context)
@@ -42,13 +47,16 @@ def report(request):
             return HttpResponseRedirect('/ataskaitos/report/')
     else:
         form = ReportForm() 
-   
+    num_reports_of_day = Report.objects.filter(date_field__contains=datetime.today().date()).count()
+    num_reports_of_month = Report.objects.filter(user=request.user).filter(date_field__contains=datetime.now().month).aggregate(Sum('number_of_new_plates'))
     paginator = Paginator(Report.objects.filter(user=request.user).order_by('-date_field'), 20)
     page_number = request.GET.get('page')
     paged_reports = paginator.get_page(page_number)
     context = {
         'report': paged_reports,
-        'form': form,      
+        'form': form,
+        'num_reports_of_day': num_reports_of_day, 
+        'num_reports_of_month':num_reports_of_month,     
     }
     
     return render(request, 'report.html', context=context)
