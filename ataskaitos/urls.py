898a44450bc6da516b456cd52360b1f6cd6d0b1d
Artūras Tkačiovas <arturas.tkaciovas@gmail.com>
2022-08-20 22:08:29 +0300

    pašalinau nereikalingus urls, perport eksperimentai su forma

--- ataskaitos/urls.py
+++ ataskaitos/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('report/', views.report, name='report'),
-    path('', views.base, name='base'),
-    path('report/', views.base, name='base'),
+    # path('', views.base, name='base'),
+    # path('report/', views.base, name='base'),
+    path('register/', views.register, name='register'),
 ]
