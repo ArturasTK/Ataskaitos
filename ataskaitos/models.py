a721a5a8d46954dd705fed25797970638e02b386
Artūras Tkačiovas <arturas.tkaciovas@gmail.com>
2022-08-20 22:04:28 +0300

    pridėjau prie progukto purpose

--- ataskaitos/models.py
+++ ataskaitos/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from datetime import datetime
+from datetime import date
 from django.contrib.auth.models import User
 
 class Product(models.Model):
     product_name = models.CharField('Produktas', max_length=50)
     price = models.DecimalField('Įkainis', max_digits=10, decimal_places=2) 
+    purpose = models.CharField('paskirtis', max_length=50, null=True, blank=True)
 
     def __str__(self):
-       return str(self.product_name) + ": €" + str(self.price)
+       return str(self.product_name) 
+    #    + ": €" + str(self.price)
 
 
 class Remake(models.Model):
@@ -28,7 +31,7 @@ class Report(models.Model):
     date_field = models.DateTimeField('Data', default=datetime.now)
     dtk_nr = models.CharField('DTK Nr.', max_length=10)
     product_name = models.ForeignKey(Product, help_text='Pasirinkite produktą', on_delete=models.SET_NULL, null=True)
-    remakes = models.ForeignKey(Remake, help_text='Pasirinkite perdarimo priežastis', on_delete=models.CASCADE, blank=True)
+    remakes = models.ForeignKey(Remake, help_text='Pasirinkite perdarimo priežastis', on_delete=models.CASCADE, null=True, blank=True)
     number_of_plates = models.PositiveIntegerField('Klišių sk.')
     number_of_new_plates = models.PositiveIntegerField('Keičiamų klišių sk.') 
     reasons = models.ForeignKey(Reason, help_text='Pasirinkite korekcijos tipą', on_delete=models.SET_NULL, null=True, blank=True)
@@ -37,6 +40,15 @@ class Report(models.Model):
     
     def __str__(self):
         return f'{self.date_field} {self.dtk_nr}'
+
+    @property
+    def is_overdue(self):
+        if self.due_back and date.today() > self.due_back:
+            return True
+        return False
+
+
+
 
        
 
